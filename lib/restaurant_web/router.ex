b19700350b01f3c1ac24b940c9eba37f35f8bd1e
guuholielixir <gustavooliveiraelixir@gmail.com>
2019-06-28 07:52:53 -0300

    first commit

---
+++ lib/restaurant_web/router.ex
@@ -0,0 +1,26 @@
+defmodule RestaurantWeb.Router do
+  use RestaurantWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", RestaurantWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", RestaurantWeb do
+  #   pipe_through :api
+  # end
+end
